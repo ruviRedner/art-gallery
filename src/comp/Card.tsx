8e0831a01a7b185fab45e237b7999d0c1b62c475
ruviRedner <r.y.r7966@gmail.com>
2024-10-30 16:25:23 +0200

    done

--- src/comp/Card.tsx
+++ src/comp/Card.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
-const Card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useState } from "react";
+import { Image } from "../App";
+interface Props {
+  image: Image;
+  onClick: (img: Image) => void;
 }
+const Card = ({ image, onClick }: Props): JSX.Element => {
+  const [like, setLike] = useState<boolean>(false);
+  const liked = (e:React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault()
+    setLike(!like)
+  }
+  return (
+    <div className="card">
+      <img src={image.urls.small} alt="" onClick={() => onClick(image)} />
+      <button onClick={liked}>{like ? "❤️" : "👍"}</button>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
